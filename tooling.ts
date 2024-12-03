79ef2e5c949e53f9dd96e536af12f512072805d4
Larry <cluckyx@gmail.com>
2024-12-03 11:31:02 +0000

    Adding npm, ts, day2 data and parse tooling

---
+++ tooling.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from "fs";
+
+export function initialDataParse(
+  file: string,
+  type: string,
+  param = 2,
+  fmt = "int"
+): string[] | (number | string)[][] {
+  const input = readFileSync(file, "utf-8");
+  if (type === "lines") {
+    return input.trim().split(`\n`);
+  } else if (type === "cols") {
+    const columns: (number | string)[][] = [];
+    const removedNewlines = input.trim().replace("\n", " ").split(" ");
+    for (let i = 0; i < param; i++) {
+      columns.push([]);
+    }
+    let colNum = 0;
+    for (const k of removedNewlines) {
+      if (colNum >= param) {
+        colNum = 0;
+      }
+      columns[colNum].push(fmt == "int" ? Math.abs(Number(k)) : k);
+      colNum++;
+    }
+    return columns;
+  }
+  return [];
+}
